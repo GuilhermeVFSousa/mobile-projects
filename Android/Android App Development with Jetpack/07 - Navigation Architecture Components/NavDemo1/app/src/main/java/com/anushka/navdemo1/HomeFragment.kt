dbd8b277739413872aebbb5db0afbc21a3635f51
Guilherme Sousa <guilherme@scenario.ind.br>
2024-10-10 15:23:27 -0300

    navigation architecture components

---
+++ Android/Android App Development with Jetpack/07 - Navigation Architecture Components/NavDemo1/app/src/main/java/com/anushka/navdemo1/HomeFragment.kt
@@ -0,0 +1,62 @@
+package com.anushka.navdemo1
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.anushka.navdemo1.databinding.FragmentHomeBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.button.setOnClickListener {
+
+            if (!TextUtils.isEmpty(binding.editTextText.text.toString())) {
+                val bundle = bundleOf("user_input" to binding.editTextText.text.toString())
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
+            } else {
+                Toast.makeText(activity, "Preencha o campo!", Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
